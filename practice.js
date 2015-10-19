3020f156a4a4f1dfa8af07f32e73f048173da5e4
Timothy Andrew Gass <timothyagass@gmail.com>
2015-10-19 09:21:46 -0500

    should work

---
+++ practice.js
@@ -0,0 +1,26 @@
+var name = 'will';
+
+// Complete the if statement so that the console.log statement runs
+// and says "Hello will";
+// You must leave the === in the expression
+
+if(name === 'will'){
+  console.log('Hello ' + name);
+}
+
+// Complete the if statement so that the console.log statement runs
+// and says "Goodbye will";
+// You must leave the bang in the conditional expression
+if (!false) {
+  console.log('Goodbye ' + );
+}
+
+// Complete the if/else statement so that the console.log statement runs
+// and says "You entered the else statement";
+// You must leave the bang in the conditional expression
+//
+if (2 != 3) {
+  console.log('Ooops!');
+} else {
+  console.log('You entered the else statement');
+}
